1a0242a2ed1a1b10e1354bb1f619c92df7d7fc1e
Jesus Figueredo <jafigueredo@gmail.com>
2021-06-26 13:21:00 -0300

    install gh-pages

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import StoreProvider from './store';
 import NavBar from './components/NavBar';
 import routes from './routes';
